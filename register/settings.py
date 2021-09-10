f0161658ad8710fa72e6c131d5b5df0c7c8285a9
Quentas <quentas320@gmail.com>
2021-09-10 18:33:51 +0300

    whitenoise static added

--- register/settings.py
+++ register/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
 
     'users',
 
+    'whitenoise',
+
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -49,6 +51,7 @@ KOSTYL = int(os.environ.get('KOSTYL', default=1))
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -181,6 +184,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
